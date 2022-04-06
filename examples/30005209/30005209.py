3c0725f26a6de679ab218e927152fd4522993d36
student <student@debian>
2022-04-07 00:37:44 +0100

    Merge issues

--- examples/30005209/30005209.py
+++ examples/30005209/30005209.py
@@ -410,7 +410,6 @@ def main ():
     pge.register_handler (key_pressed, [KEYDOWN])
     chair(0.2,0.2,0.05, 0.05, True)
     chair(0.1,0.1,0.05, 0.05, False)
-    chair(0.2,0.2, 0.05, 0.05, True)
     display_score()
     place_gun()
 
